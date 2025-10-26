704c04e90d7d90eb3767122645b63e34d9c88e04
Hog <156556823+HogosoKrikoso@users.noreply.github.com>
2025-10-26 18:33:04 -0400

    ralsei update

--- data/chars/ralsei.hx
+++ data/chars/ralsei.hx
@@ -11,8 +11,13 @@ var character = new DeltaCharacter("Ralsei", "ralsei", "characters/ralsei", [
 	{
 		text: "Heal Prayer",
 		tp: 32,
-		player: true,
-		func: () -> {},
+		party: true,
+		func: (charA, charB) -> {
+			events[eventName].push(() -> doTextStuff("* Ralsei heals " + charB.name + ".", false, true, "default", null, 0.05));
+			events[eventName].push(() -> {
+				charA.playAnim("heal", true);
+			});
+		},
 	},
 ], 0, 0);
 characters.push(character);
