0b7f8673c42793fdacc41c4a84a24f17bcbd49c6
Hog <156556823+HogosoKrikoso@users.noreply.github.com>
2025-10-28 20:19:20 -0400

    Queso

--- data/chars/ralsei.hx
+++ data/chars/ralsei.hx
@@ -1,21 +1,173 @@
 import deltaflixel.objects.DeltaCharacter;
 
+function framesDuration(fps, frames) return (1 / fps) * frames;
+
 var character = new DeltaCharacter("Ralsei", "ralsei", "characters/ralsei", [
 	{
 		name: "idle",
 		prefix: "idle",
 		fps: 12,
 		loop: true,
+		endFrame: 5,
 	},
-], 4, 140, 0xFF77FF00, true, [
+	{
+		name: "dead",
+		prefix: "dead",
+		fps: 1,
+		loop: true,
+	},
+	{
+		name: "attackprep",
+		prefix: "attackprep",
+		fps: 1,
+		loop: true,
+		offset: [0, 10],
+	},
+	{
+		name: "actprep",
+		prefix: "actprep",
+		fps: 4,
+		loop: true,
+	},
+	{
+		name: "itemprep",
+		prefix: "itemprep",
+		fps: 4,
+		loop: true,
+	},
+	{
+		name: "spellprep",
+		prefix: "spellprep",
+		fps: 12,
+		loop: true,
+		offset: [0, 5],
+	},
+	{
+		name: "spell",
+		prefix: "spell",
+		fps: 12,
+		loop: false,
+		offset: [0, 5],
+		indices: [0,1,2,3,4,5,6,7],
+	},
+	{
+		name: "attack",
+		prefix: "attack",
+		fps: 24,
+		loop: false,
+		offset: [0, 10],
+	},
+	{
+		name: "defend",
+		prefix: "defend",
+		fps: 12,
+		loop: false,
+	},
+	{
+		name: "hurt",
+		prefix: "hurt",
+		fps: 1,
+		loop: false,
+	},
+	{
+		name: "swordjump",
+		prefix: "swordjump",
+		fps: 12,
+		loop: true,
+	},
+	{
+		name: "battlestart",
+		prefix: "battlestart",
+		fps: 12,
+		loop: false,
+	},
+	{
+		name: "victory",
+		prefix: "victory",
+		fps: 12,
+		loop: false,
+	},
+	{
+		name: "u_idle",
+		prefix: "walk_overworld_up",
+		fps: 12,
+		loop: true,
+		endFrame: 0,
+	},
+	{
+		name: "r_idle",
+		prefix: "walk_overworld_right",
+		fps: 12,
+		loop: true,
+		endFrame: 0,
+	},
+	{
+		name: "d_idle",
+		prefix: "walk_overworld_down",
+		fps: 12,
+		loop: true,
+		endFrame: 0,
+	},
+	{
+		name: "l_idle",
+		prefix: "walk_overworld_left",
+		fps: 12,
+		loop: true,
+		endFrame: 0,
+	},
+	{
+		name: "u_walk",
+		prefix: "walk_overworld_up",
+		fps: 6,
+		loop: true,
+		endFrame: 3,
+	},
+	{
+		name: "r_walk",
+		prefix: "walk_overworld_right",
+		fps: 6,
+		loop: true,
+		endFrame: 3,
+	},
+	{
+		name: "d_walk",
+		prefix: "walk_overworld_down",
+		fps: 6,
+		loop: true,
+		endFrame: 3,
+	},
+	{
+		name: "l_walk",
+		prefix: "walk_overworld_left",
+		fps: 6,
+		loop: true,
+		endFrame: 3,
+	},
+], 4, 180, 0xFF77FF00, true, [
 	{
 		text: "Heal Prayer",
 		tp: 32,
 		party: true,
 		func: (charA, charB) -> {
-			events[eventName].push(() -> doTextStuff("* Ralsei heals " + charB.name + ".", false, true, "default", null, 0.05));
+			var charB = charB;
+			var charA = charA;
+			events[eventName].push(() -> doTextStuff("* Ralsei casts Heal Prayer.", false, true, "default", null, 0.05));
 			events[eventName].push(() -> {
-				charA.playAnim("heal", true);
+				charA.playAnim("spell", true);
+				new FlxTimer().start(framesDuration(12, 2), () -> FlxTween.tween(charB.colorTransform, {redOffset: 255, greenOffset: 255, blueOffset: 255}, framesDuration(12, 5), {ease: FlxEase.quadIn, onComplete: handleEvent}));
+			});
+			events[eventName].push(() -> {
+				charB.heal(70);
+				charA.playAnim("spellend", true);
+				FlxTween.tween(charB.colorTransform, {redOffset: 0, greenOffset: 0, blueOffset: 0}, 0.32, {ease: FlxEase.quadOut, onComplete: handleEvent});
+			});
+			events[eventName].push(() -> {
+				conditionFunction = () -> {
+					if(controls.ACCEPT) {
+						endDialouge(false);
+						return true;
+					}
+				}
 			});
 		},
 	},
--- data/chars/susie.hx
+++ data/chars/susie.hx
@@ -1,8 +1,12 @@
 import flixel.math.FlxAngle;
 import deltaflixel.objects.DeltaCharacter;
 
-function framesDuration(fps, frames)
-	return (1 / fps) * frames;
+function framesDuration(fps, frames) return (1 / fps) * frames;
+
+function playSound(path, ?force:Bool = false) {
+	var sound = FlxG.sound.load(Paths.sound(path));
+	sound.play(force);
+}
 
 var character = new DeltaCharacter("Susie", "susie", "characters/susie_noeyes", [
 	{
@@ -28,39 +32,42 @@ var character = new DeltaCharacter("Susie", "susie", "characters/susie_noeyes", 
 		prefix: "rb_spell",
 		fps: 12,
 		loop: true,
-		offset: [0, 29]
+		offset: [0, 129]
 	},
 	{
 		name: "buster",
 		prefix: "rb_cast",
 		fps: 12,
 		loop: true,
-		offset: [18, 32]
+		offset: [18, 132]
 	},
 ], 4, 230, 0xFFFF00FF, true, [
 	{
 		text: "Rude Buster",
 		tp: 0,
 		info: "Rude damage.",
-		func: function(character, enemy) {
-			var enemy = enemies[0];
+		func: (character, enemy) -> {
+			var enemy = enemy;
+			var character = character;
 			events[eventName].push(() -> doTextStuff("* Susie casts Rude Buster!" + (enemy != null), false, true, "default", null, 0.05));
 			events[eventName].push(() -> {
-				character.sprite.playAnim("rude");
+				character.playAnim("rude");
 				new FlxTimer().start(framesDuration(12, 8), (tmr) -> {
-					character.sprite.playAnim("buster");
+					character.playAnim("buster");
 					new FlxTimer().start(framesDuration(12, 6), (tmr) -> {
-						rudebuster = new FunkinSprite(character.sprite.x, character.sprite.y, Paths.image('rudebuster_single'));
+						rudebuster = new FunkinSprite(character.x, character.y, Paths.image('rudebuster_single'));
 						rudebuster.scale.set(4, 4);
 						rudebuster.updateHitbox();
 						rudebuster.offset.x = 120;
 						rudebuster.offset.y = 25;
-						rudebuster.angle = FlxAngle.degreesBetween(character.sprite, enemy.sprite);
+						rudebuster.angle = FlxAngle.degreesBetween(character, enemy);
 						rudebuster.cameras = [overworldFront];
 						add(rudebuster);
-						FlxTween.tween(rudebuster, {x: enemy.sprite.x, y: enemy.sprite.y}, 1, {onComplete: () -> {
+						playSound("rudebuster_swing");
+						FlxTween.tween(rudebuster, {x: enemy.x, y: enemy.y}, 0.32, {onComplete: () -> {
 							remove(rudebuster);
-							var splash = new FunkinSprite(enemy.sprite.x, enemy.sprite.y, Paths.image('rudebuster_splash'));
+							playSound("rudebuster_hit");
+							var splash = new FunkinSprite(enemy.x, enemy.y, Paths.image('rudebuster_splash'));
 							splash.addAnim("play", "play", 20, false);
 							splash.scale.set(3, 3);
 							splash.updateHitbox();
@@ -68,25 +75,22 @@ var character = new DeltaCharacter("Susie", "susie", "characters/susie_noeyes", 
 							splash.cameras = [overworldFront];
 							add(splash);
 							splash.playAnim("play", true);
-							enemy.shake = 20;
-							enemy.hp -= FlxG.random.int(150,200);
-							enemy.sprite.playAnim("hurt", true);
+							enemy.hurt(FlxG.random.int(150,200), true, 20);
 							new FlxTimer().start(0.32, (tmr) -> {
-								enemy.sprite.playAnim("idle", true);
+								enemy.playAnim("idle", true);
 								handleEvent();
 							});
 						}});
 					});
 					new FlxTimer().start(framesDuration(12, 14), (tmr) -> {
-						character.sprite.playAnim("idle");
-						character.sprite.offset.set(0,0);
+						character.playAnim("idle");
 					});
 				});
 			});
 			events[eventName].push(() -> {
 				conditionFunction = () -> {
 					if (controls.ACCEPT) {
-						endDialouge();
+						endDialouge(false);
 						return true;
 					}
 					return false;
