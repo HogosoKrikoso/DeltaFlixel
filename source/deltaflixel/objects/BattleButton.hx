314f2837aff62fd8dffdde1bc209e6456da6b780
Hog <156556823+HogosoKrikoso@users.noreply.github.com>
2025-10-30 18:39:35 -0400

    Add files via upload

--- source/deltaflixel/objects/BattleButton.hx
+++ source/deltaflixel/objects/BattleButton.hx
@@ -1,6 +1,7 @@
 class BattleButton extends FunkinSprite
 {
 	public var buttonName:String = "";
+	public var forceGraphicName:String = "";
 	public function new(X, Y, ButtonName, Scale) {
 		super(X,Y);
 		scale.set(Scale,Scale);
@@ -8,7 +9,7 @@ class BattleButton extends FunkinSprite
 		updateGraphic(false);
 	}
 	public function updateGraphic(highlight){
-		loadGraphic(Paths.image("ui/" + buttonName + (highlight ? "_h" : "")));
+		loadGraphic(Paths.image("ui/" + (forceGraphicName != "" ? forceGraphicName : buttonName) + (highlight ? "_h" : "")));
 		updateHitbox();
 	}
 }
