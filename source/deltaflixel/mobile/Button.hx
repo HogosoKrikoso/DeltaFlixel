b8fada576648c8adbddb380bbec7a1dfcbbe9c6c
Hog <156556823+HogosoKrikoso@users.noreply.github.com>
2025-11-02 14:32:54 -0400

    le button

---
+++ source/deltaflixel/mobile/Button.hx
@@ -0,0 +1,33 @@
+class Button extends FlxSprite
+{
+	public var justPressed = false;
+	public var pressed = false;
+	public var justReleased = false;
+	public var buttonColor:FlxColor = FlxColor.WHITE;
+	
+	public function new(x = 0.0, y = 0.0, ?graphic:String, ?color:FlxColor)
+	{
+		super(x, y);
+		loadGraphic(Paths.image(graphic));
+		if (color != null) buttonColor = color;
+	}
+	
+	public function update(?elapsed)
+	{
+		if (pressed) {
+			if(justPressed) justPressed = false;
+			if (!FlxG.mouse.pressed || !FlxG.mouse.overlaps(this, camera)) {
+				pressed = false;
+				justReleased = true;
+			}
+			color = FlxColor.YELLOW;
+		} else {
+			if(justReleased) justPressed = false;
+			if (FlxG.mouse.overlaps(this, camera) && FlxG.mouse.pressed) {
+				justPressed = true;
+				pressed = true;
+			}
+			color = buttonColor;
+		}
+	}
+}
